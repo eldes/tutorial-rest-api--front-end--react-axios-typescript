2963738845159353388aeaecc7d073da62c6a7c4
Eldes <studio@eldes.com>
2021-11-28 22:30:31 -0300

    [+] roteamento de páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ItensPage from './pages/itens'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return(
+		<BrowserRouter>
+			<Routes>
+				<Route path="/itens" element={<ItensPage/>} />
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
-export default App;
+export default App
---
+++ src/pages/itens.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from 'react'
+
+const ItensPage: FunctionComponent = () => {
+	return (
+		<>
+			<h1>Itens</h1>
+		</>
+	)
+}
+
+export default ItensPage
